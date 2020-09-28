b80c034d940f81addd20b3dca30f8c0a9622ff20
UMuhitdinov <usmon1983@gmail.com>
2020-09-28 16:58:51 +0500

    добавлена функция PeriodsDynamic и автотест TestPeriodsDynamicUsers

--- pkg/stats/stats_test.go
+++ pkg/stats/stats_test.go
@@ -26,3 +26,23 @@ func TestCategoriesTotal(t *testing.T)  {
 		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
 	}
 }
+
+func TestPeriodsDynamicUsers(t *testing.T) {
+	first := map[types.Category]types.Money {
+		"auto": 1_000_00,
+		"food": 2_000_00,
+	}
+	second := map[types.Category]types.Money {
+		"auto": 2_000_00,
+		"food": 3_000_00,
+	}
+	expected := map[types.Category]types.Money {
+		"auto": 1_000_00,
+		"food": 1_000_00,
+	}
+    
+	result := PeriodsDynamic(first,second)
+	if !reflect.DeepEqual(expected,result) {
+		t.Errorf("invalid result, expected: %v, actual: %v", expected, result)
+	}
+}
